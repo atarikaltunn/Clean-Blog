24ac7889bb0419b0a997c5bab12943b483dba300
atarikaltunn <atarikaltunn@gmail.com>
2022-04-24 13:43:44 +0300

    template engine added

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-const express = require("express");
+const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+//VIEW ENGINE
+app.set('view engine', 'ejs');
 
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    res.send(blog);
-})
+    res.render('index');
+});
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+
+
 
 const port = 3000;
-app.listen(port, () =>  {
-    console.log(`Server ${port} portunda başlatıldı...`)
-})
+app.listen(port, () => {
+    console.log(`Server ${port} portunda başlatıldı...`);
+});
