a790e26dcb7a34c787dccf3e0b7894307630cdd7
atarikaltunn <atarikaltunn@gmail.com>
2022-04-24 23:45:28 +0300

    dynamic data add and showing them was added.

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Posts = require('./models/posts');
 
 const app = express();
 
-//VIEW ENGINE
+//DB CONNECTION
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const posts = await Posts.find({});
+    res.render('index', {
+        posts,
+    });
 });
 app.get('/about', (req, res) => {
     res.render('about');
@@ -19,9 +32,10 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     res.render('add');
 });
-
-
-
+app.post('/posts', async (req, res) => {
+    await Posts.create(req.body);
+    res.redirect('/');
+});
 
 const port = 3000;
 app.listen(port, () => {
